8b3bde22c0db238c7bf0b21409adac4f07d51e55
Mariano Casco <mariano.c88@gmail.com>
2013-12-30 03:03:52 -0300

    apply

--- main.hs
+++ main.hs
@@ -125,9 +125,37 @@ eval val@(Atom _)               = val
 eval val@(Char _)               = val
 eval val@(Number _)             = val
 eval val@(Bool _)               = val
-eval (List [Atom "quote", val]) = val
-eval val@(List _)               = val
 eval val@(DottedList _ _)       = val
+eval (List [Atom "quote", val]) = val
+eval (List (Atom func : args))  = apply func $ map eval args
+eval val@(List _)               = val
+
+apply :: String -> [LispVal] -> LispVal
+apply f args = maybe (Bool False) ($ args) $ lookup f primitives
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives = [("+", numericBinop (+)),
+              ("-", numericBinop (-)),
+              ("*", numericBinop (*)),
+              ("/", numericBinop div),
+              ("mod", numericBinop mod),
+              ("quotient", numericBinop quot),
+              ("remainder", numericBinop rem)]
+
+numericBinop :: (Integer -> Integer -> Integer)
+             -> [LispVal]
+             -> LispVal
+numericBinop f = Number . foldl1 f . map unpackNum
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n)    = n
+unpackNum (String n)    =
+    let parsed = reads n :: [(Integer, String)] in
+        if null parsed
+          then 0
+          else fst $ head parsed
+unpackNum (List [n])    = unpackNum n
+unpackNum _             = 0
 
 readExpr :: String -> LispVal
 readExpr input =
